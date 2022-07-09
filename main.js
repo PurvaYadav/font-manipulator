faa15339390104dc3dbb95d225a641d4436cb238
PurvaYadav <94224975+PurvaYadav@users.noreply.github.com>
2022-07-09 17:16:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,45 +1,43 @@
-noseX = 0;
-noseY = 0;
-difference =0;
-rightWristX =0;
-leftWristX =0;
 
-function setup() {
-    video = createCapture(VIDEO);
-    video.size(450,400);
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
-    canvas = createCanvas(550,500);
-    canvas.position(560,150);
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose' ,gotPoses);
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
-    console.log('PoseNet is Initialised');
+  console.log('PoseNet Is Initialized!');
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
 
 function draw() {
-    background('#96979A');
-    document.getElementById("text_side").innerHTML = "Width And Height Of The Text Will Be = "+difference+"px";
-    fill('#F90099');
-    stroke('#F90099');
-    text('Text',noseX ,noseY,difference); 
-}
+background('#969A97');
 
-function gotPoses(results) 
-{
-    if (results.length>0)
-    {
-        console.log(results);
-        noseX = results[0].pose.nose.x;
-        noseY = results[0].pose.nose.y;
-        console.log("noseX = "+noseX+ "noseY"+noseY);
-        
-        leftWristX = results[0].pose.leftWrist.x;
-        rightWristX = results[0].pose.rightWrist.x;
-        difference = floor(leftWristX - rightWristX); 
-        
-        console.log("leftWristX ="+leftWristX+ "rightWristX ="+rightWristX+ "difference =" +difference);
-    }
+  document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+textSize(difference);
+fill('#FFE787');
+text('Hi World', 50, 400);
 }
